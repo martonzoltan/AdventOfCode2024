6cd16176a66600c28c10ecaf9a87a279a0229597
Zoltan Marton <martonzoltan4@gmail.com>
2024-12-04 21:57:22 +0200

    Solve day 4

---
+++ AdventOfCode/Day04.cs
@@ -0,0 +1,107 @@
+﻿using Core;
+
+namespace AdventOfCode;
+
+public sealed class Day04 : BaseDay
+{
+    private readonly char[,] _matrix;
+
+    public Day04()
+    {
+        _matrix = InputHelper.GetMatrixInput(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var totalXmas = 0;
+        for (var i = 0; i < _matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < _matrix.GetLength(1); j++)
+            {
+                foreach (var (di, dj) in MatrixHelper.Directions)
+                {
+                    if (Verify(i, j, di, dj, "XMAS"))
+                    {
+                        totalXmas++;
+                    }
+                }
+            }
+        }
+
+        return new ValueTask<string>($"{totalXmas}");
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var totalMas = 0;
+        for (var i = 0; i < _matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < _matrix.GetLength(1); j++)
+            {
+                if (_matrix[i, j] != 'A') continue;
+                if (VerifyDiagonalMas(i, j))
+                {
+                    totalMas++;
+                }
+            }
+        }
+
+        return new ValueTask<string>($"{totalMas}");
+    }
+
+    private bool Verify(int i, int j, int di, int dj, string target, int index = 0)
+    {
+        while (true)
+        {
+            if (index >= target.Length) return true;
+            if (!MatrixHelper.IsInsideMatrix(_matrix, i, j)) return false;
+            if (_matrix[i, j] != target[index]) return false;
+            i += di;
+            j += dj;
+            index += 1;
+        }
+    }
+
+    private bool VerifyDiagonalMas(int i, int j)
+    {
+        if (!MatrixHelper.IsInsideMatrix(_matrix, i - 1, j - 1) ||
+            !MatrixHelper.IsInsideMatrix(_matrix, i + 1, j + 1) ||
+            !MatrixHelper.IsInsideMatrix(_matrix, i - 1, j + 1) ||
+            !MatrixHelper.IsInsideMatrix(_matrix, i + 1, j - 1))
+            return false;
+
+        if (_matrix[i - 1, j - 1] == 'M' &&
+            _matrix[i + 1, j + 1] == 'S' &&
+            _matrix[i - 1, j + 1] == 'M' &&
+            _matrix[i + 1, j - 1] == 'S')
+        {
+            return true;
+        }
+
+        if (_matrix[i - 1, j - 1] == 'S' &&
+            _matrix[i + 1, j + 1] == 'M' &&
+            _matrix[i - 1, j + 1] == 'S' &&
+            _matrix[i + 1, j - 1] == 'M')
+        {
+            return true;
+        }
+
+        if (_matrix[i - 1, j - 1] == 'S' &&
+            _matrix[i + 1, j + 1] == 'M' &&
+            _matrix[i - 1, j + 1] == 'M' &&
+            _matrix[i + 1, j - 1] == 'S')
+        {
+            return true;
+        }
+
+        if (_matrix[i - 1, j - 1] == 'M' &&
+            _matrix[i + 1, j + 1] == 'S' &&
+            _matrix[i - 1, j + 1] == 'S' &&
+            _matrix[i + 1, j - 1] == 'M')
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
--- Core/MatrixHelper.cs
+++ Core/MatrixHelper.cs
@@ -2,6 +2,18 @@ namespace Core;
 
 public class MatrixHelper
 {
+    public static readonly (int di, int dj)[] Directions =
+    [
+        (-1, 0), // Up
+        (-1, 1), // DiagTopRight
+        (0, 1), // Right
+        (1, 1), // DiagBottomRight
+        (1, 0), // Down
+        (1, -1), // DiagBottomLeft
+        (0, -1), // Left
+        (-1, -1) // DiagTopLeft
+    ];
+
     public static bool IsInsideMatrix<T>(T[,] matrix, int i, int j)
     {
         return i >= 0 && j >= 0 && i < matrix.GetLength(0) && j < matrix.GetLength(1);
